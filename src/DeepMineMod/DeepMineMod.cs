a68985a2c480ff64d62c4dfe622888e3dbbe71f4
Christopher Eli Oswald <8730580+Christopher-Eli-Oswald@users.noreply.github.com>
2026-08-04 19:52:21 -0700

    Add Deep Mine mod entry point

---
+++ src/DeepMineMod/DeepMineMod.cs
@@ -0,0 +1,20 @@
+using Mafi;
+using Mafi.Base;
+using Mafi.Collections;
+using Mafi.Core;
+using Mafi.Core.Mods;
+
+namespace DeepMineMod;
+
+public sealed class DeepMineMod : DataOnlyMod {
+    public DeepMineMod(ModManifest manifest) : base(manifest) {
+        Log.Info("DeepMineMod: loaded");
+    }
+
+    public override void RegisterPrototypes(ProtoRegistrator registrator) {
+        Log.Info("DeepMineMod: prototype registration complete");
+    }
+
+    public override void MigrateJsonConfig(VersionSlim savedVersion, Dict<string, object> savedValues) {
+    }
+}
